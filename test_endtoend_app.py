ce102e362d3929cd410fd72a60ec21b8257b4d1e
gabgrge <gabriel.george@efrei.net>
2024-01-11 16:06:16 +0100

    Updated

--- test_endtoend_app.py
+++ test_endtoend_app.py
@@ -15,8 +15,15 @@ class TestAppE2E(unittest.TestCase):
         # Parse the response HTML using BeautifulSoup
         soup = BeautifulSoup(response.text, 'html.parser')
 
+        # Print the HTML response
+        print(soup.prettify())
+
         # Find the new item in the HTML
         item = soup.find('li', text='New E2E Item')
+
+        # Print the item
+        print('Item:', item)
+
         self.assertIsNotNone(item)
 
         # Extract the item index from the delete link URL
